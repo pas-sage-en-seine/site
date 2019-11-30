10da759d7b82bb1e5346544e9986506a2ec52f9c
aeris <aeris@imirhil.fr>
2019-11-30 18:53:17 +0100

    Better modal

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,25 +1,44 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const modalBackground = document.querySelector("#modal-background")
-  // const close = modalBackground.querySelector(".close")
-  modalBackground.addEventListener("click", () => {
-    const modals = document.querySelectorAll("[data-modal].show")
-    modals.forEach((m) => {
-      m.classList.remove("show")
-      m.classList.add("hide")
-    })
-    modalBackground.classList.remove("show")
-  })
+  window.modals = {
+    init: function () {
+      this.modalBackground = document.querySelector("#modal-background")
+      this.closeButton = this.modalBackground.querySelector(".close")
 
-  document.querySelectorAll("[data-modal-target]").forEach((e) => {
-    const target = e.dataset.modalTarget
-    const modal = document.querySelector(`[data-modal="${target}"]`)
-    modalBackground.appendChild(modal)
+      this.modalBackground.addEventListener("click", () => {
+        const modals = document.querySelectorAll("[data-modal].show")
+        modals.forEach(m => this.close(m))
+        this.modalBackground.classList.remove("show")
+      })
 
-    e.addEventListener("click", () => {
-      modalBackground.classList.add("show")
+      document.querySelectorAll("[data-modal-target]").forEach(e => {
+        const target = e.dataset.modalTarget
+        const modal = this.get(target)
+        this.modalBackground.appendChild(modal)
+        e.addEventListener("click", () => this.open(modal))
+      })
 
+      const target = window.location.hash.substr(1)
+      const modal = this.get(target)
+      if (modal) this.open(modal)
+    },
+
+    get: function (name) {
+      return document.querySelector(`[data-modal="${name}"]`)
+    },
+
+    open: function (modal) {
+      modal.appendChild(this.closeButton)
+      this.modalBackground.classList.add("show")
       modal.classList.remove("hide")
       modal.classList.add("show")
-    })
-  })
+    },
+
+    close: function (modal) {
+      modal.classList.remove("show")
+      modal.classList.add("hide")
+      this.modalBackground.appendChild(this.closeButton)
+    }
+  }
+
+  window.modals.init()
 })
